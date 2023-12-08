c4c21d260f74d9ddf2f11c8d2f65b6a01e6f583d
Prithwish Dan <dan.prithwish@gmail.com>
2023-12-08 14:45:02 -0500

    Refactored evaluation

--- eval_zeroshot_pokemon.py
+++ eval_zeroshot_pokemon.py
@@ -20,31 +20,34 @@ for example in test_dataset:
     names.append(example[1][0])
     name_idxs.append(example[1][1])
 
+model.eval()
+text_inputs = torch.cat([
+    clip.tokenize(f"a photo of Pokemon named {c}")
+    for c in test_dataset.pokemon_names["name"].tolist()
+]).to(device)
+with torch.no_grad():
+    text_features = model.encode_text(text_inputs)
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+
 # Loop through the test set in batches
 for i in range(0, total_samples, batch_size):
     images = torch.stack([preprocess(image) for image in imgs[i:i+batch_size]]).to(device)
-    text_inputs = torch.cat([
-        clip.tokenize(f"a photo of Pokemon named {c}")
-        for c in list(test_dataset.pokemon_names["name"])
-    ]).to(device)
-
+    
     # Calculate features
     with torch.no_grad():
         image_features = model.encode_image(images)
-        text_features = model.encode_text(text_inputs)
-
+        
     # Normalize features
     image_features /= image_features.norm(dim=-1, keepdim=True)
-    text_features /= text_features.norm(dim=-1, keepdim=True)
 
     # Calculate similarity
     similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
     # Get the top prediction for each sample in the batch
-    top_predictions = similarity.argmax(dim=-1).tolist()
+    top_predictions = similarity.softmax(dim=-1).topk(1)
 
     # Check if the top predictions are correct
     for j in range(batch_size):
-        if top_predictions[j] == name_idxs[i:i+j]:
+        if name_idxs[i+j] in top_predictions[j]:
             correct_predictions += 1
 
 # Calculate accuracy
